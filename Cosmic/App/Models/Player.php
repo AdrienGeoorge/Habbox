c056860efb6d1ac3f14b1b883799cd6f0a10ded6
AdrienGeoorge <adrien-george@outlook.fr>
2021-11-22 21:54:43 +0100

    Calendrier de l'avent: correctifs

--- Cosmic/App/Models/Player.php
+++ Cosmic/App/Models/Player.php
@@ -329,16 +329,16 @@ class Player
         if ($received) {
             return ['already-received'];
         } else {
-            QueryBuilder::connection()->table('christmas_gifts')->insert(array('user' => $player->id, 'day' => (int)date('jS'), 'year' => (int)date('Y')));
+//            QueryBuilder::connection()->table('christmas_gifts')->insert(array('user' => $player->id, 'day' => (int)date('jS'), 'year' => (int)date('Y')));
             switch ($type) {
                 case 'diamonds':
-                    HotelApi::execute('givepoints', array('user_id' => $player->id, 'points' => (int)$amount, 'type' => 'diamonds'));
+                    HotelApi::execute('givepoints', array('user_id' => $player->id, 'points' => + (int)$amount, 'type' => 'diamonds'));
                     return ['ok', sprintf('Tu as bien reçu ton cadeau! En attendant Noël, nous t\'offrons %s diamants à dépenser dans la boutique', $amount)];
                 case 'duckets':
-                    HotelApi::execute('givepoints', array('user_id' => $player->id, 'points' => (int)$amount, 'type' => 'duckets'));
+                    HotelApi::execute('givepoints', array('user_id' => $player->id, 'points' => + (int)$amount, 'type' => 'duckets'));
                     return ['ok', sprintf('Tu as bien reçu ton cadeau! En attendant Noël, nous t\'offrons %s duckets à dépenser dans la boutique', $amount)];
                 case 'credits':
-                    HotelApi::execute('givecredits', ['user_id' => $player->id, 'credits' => -$player->credits + (int)$amount]);
+                    HotelApi::execute('givecredits', ['user_id' => $player->id, 'credits' => $player->credits + (int)$amount]);
                     return ['ok', sprintf('Tu as bien reçu ton cadeau! En attendant Noël, nous t\'offrons %s crédits', $amount)];
                 case 'vip':
                     Player::insertMembership($player->id, $player->rank, strtotime('+' . $amount . ' days'));
