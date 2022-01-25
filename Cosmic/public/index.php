f66d63dae85cc41dbbe2940fe4cda48933760044
AdrienGeoorge <adrien-george@outlook.fr>
2022-01-26 00:14:48 +0100

    Fix bug session

--- Cosmic/public/index.php
+++ Cosmic/public/index.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *  Set session
+ */
+ini_set('session.gc_maxlifetime', 604800);
+//ini_set('session.cookie_lifetime', 604800);
+//ini_set("session.use_only_cookies", true);
+//session_cache_expire(10080);
+session_set_cookie_params(604800);
+session_start();
+
 use App\Config;
 use Core\Routes;
 use Core\QueryBuilder;
@@ -9,17 +19,6 @@ include_once __DIR__ . '/../vendor/autoload.php';
 if(Config::debug) {
     ini_set("display_errors", 1);
 }
-
-/**
- *  Set session
- */
-
-ini_set('session.gc_maxlifetime', 21600);
-ini_set('session.cookie_lifetime', 21600);
-//ini_set("session.use_only_cookies", true);
-session_cache_expire(360);
-session_set_cookie_params(21600);
-session_start();
 
 /**
  *  Set QueryBuilder
