8dede5e026a23963bdabba9f5a957596b95806f3
AdrienGeoorge <adrien-george@outlook.fr>
2022-05-15 15:05:51 +0200

    Page lorsque l'on est déconnecté de l'hôtel

--- Cosmic/public/assets/js/web/web.pages.js
+++ Cosmic/public/assets/js/web/web.pages.js
@@ -52,7 +52,37 @@ function WebHotelManagerInterface() {
             if (container.find(".client-frame").length === 0)
 
                 Web.ajax_manager.get("/api/ssoTicket", function(result) {
-                    container.prepend('<iframe class="client-frame nitro" src="' + Client.nitro_path + '/?sso=' + result.ticket + '"></iframe>');
+                    container.prepend('<iframe id="nitro" class="client-frame nitro" src="' + Client.nitro_path + '/?sso=' + result.ticket + '"></iframe>');
+
+                    let frame = document.getElementById('nitro');
+
+                    window.FlashExternalInterface = {};
+                    window.FlashExternalInterface.disconnect = () => {
+                        let errorView = document.getElementById('client-error-disconnect');
+                        errorView.classList.remove('d-none');
+                        frame.classList.add('d-none');
+                    };
+
+                    if (frame && frame.contentWindow) {
+                        window.addEventListener("message", ev => {
+                            if (!frame || ev.source !== frame.contentWindow) return;
+                            const legacyInterface = "Nitro_LegacyExternalInterface";
+                            if (typeof ev.data !== "string") return;
+                            if (ev.data.startsWith(legacyInterface)) {
+                                const {
+                                    method,
+                                    params
+                                } = JSON.parse(
+                                    ev.data.substr(legacyInterface.length)
+                                );
+                                if (!("FlashExternalInterface" in window)) return;
+                                const fn = window.FlashExternalInterface[method];
+                                if (!fn) return;
+                                fn(...params);
+                                return;
+                            }
+                        });
+                    }
                 });
 
             document.title = 'Hotel - ' + Site.name;
